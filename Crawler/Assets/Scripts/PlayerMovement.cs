4884efe9c72e9bf81832a56cf96c15286b6f73d8
scouter238 <jacobfs2009@hotmail.com>
2019-02-23 13:16:58 -0800

    Initial Comit

---
+++ Crawler/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public float movespeed;
+    public bool onWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onWall = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        if (Input.GetKey("d"))
+        {
+            position.x += movespeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey("s"))
+        {
+            position.y -= movespeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey("w"))
+        {
+            position.y += movespeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey("a"))
+        {
+            position.x -= movespeed;
+            this.transform.position = position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Wall"))
+        {
+            print("fuck)");
+            //print(collision.transform.);
+        }
+    }
+}
---
+++ Crawler/Assets/Scripts/start.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+     public void startGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void quitButton()
+    {
+
+        Application.Quit();
+
+    }
+}
