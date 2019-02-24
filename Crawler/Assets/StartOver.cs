f336427a56fe5fdc36052f2dd31c59bc116bcad7
Drew Stevens <stevensdrew42@gmail.com>
2019-02-24 00:33:49 -0800

    iuh

---
+++ Crawler/Assets/StartOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartOver : MonoBehaviour
+{
+    public void endGame()
+    {
+        SceneManager.LoadScene("StartScene");
+        PlayerMovement.startGame();
+    }
+}
